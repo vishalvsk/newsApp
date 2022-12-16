b4c0d71eaeb9c12dbe9670710d2c6d32fdfb79dc
vishal Karale <vishalkaralevsk@gmail.com>
2022-12-16 22:17:08 +0530

    done

--- news/src/components/Spinner.jsx
+++ news/src/components/Spinner.jsx
@@ -3,9 +3,9 @@ import loading from './loading.gif'
 
 const Spinner = ()=> {
         return (
-            // <div className="text-center">
-            //     <img className="my-3" src={loading} alt="loading" />
-            // </div>
+            <div className="text-center">
+                <img className="my-3" src={loading} alt="loading" />
+            </div>
         )
 }
 
